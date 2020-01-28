c88d8f190afbae7d3b5dbdba5d01cdc691efd005
devin-bielejec <devin.bieleje@gmail.com>
2020-01-28 17:05:30 -0500

    Everything but resize

--- src/hashtable.py
+++ src/hashtable.py
@@ -46,7 +46,7 @@ class HashTable:
     def insert(self, key, value):
         #put value in at random index
         if not self.storage[self._hash_mod(key)]:
-            self.storage[self._hash_mod(key)] = value
+            self.storage[self._hash_mod(key)] = LinkedPair(key, value)
         #add to head of linked list
         else:
             #make new linked Pair - adding (key, value) to head
@@ -77,24 +77,16 @@ class HashTable:
                             prev.next = current.next
                         current = current.next
 
-
     def retrieve(self, key):
         if not self.storage[self._hash_mod(key)]:
             return None
         else:
-            head = self.storage[self._hash_mod(key)]
-            #1 item
-            if not head.next:
-                return self.storage[self._hash_mod(key)]
-            #more than 1, so Linked List, head value has key
-            else:
-                #TRAVERSE LINKED LIST TIL WE FIND VALUE
-                current = head
-                while current:
-                    if current.key == key:
-                        return current
-                    current = current.next
-
+            current = self.storage[self._hash_mod(key)]
+            #TRAVERSE LINKED LIST TIL WE FIND VALUE
+            while current:
+                if current.key == key:
+                    return current.value
+                current = current.next
 
     def resize(self):
         '''
